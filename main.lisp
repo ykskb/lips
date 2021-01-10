8ce57d616f95b20994f4bf9c00f75b582e9ff5fc
ykskb <yoheikusakabe@gmail.com>
2021-01-10 20:45:36 +0800

    Add symbol reading.

--- main.lisp
+++ main.lisp
@@ -29,7 +29,7 @@
     (let ((ch (char line pos)))
       (cond ((equal ch #\Space) (misp-read line (1+ pos)))
             ((digit-char-p ch) (read-number (subseq line pos)))
-            (t "not a number")))))
+            (t (read-symbol (subseq line pos)))))))
 
 (defun read-number (line)
   (parse-integer (list-to-string (read-number-chars line))))
@@ -41,11 +41,21 @@
         (cons ch (read-number-chars line (1+ pos)))))
     nil))
 
+(defun read-symbol (line &optional (pos 0))
+  (list-to-string (read-symbol-chars line)))
+
+(defun read-symbol-chars (line &optional (pos 0))
+  (if (< pos (length line))
+    (let ((ch (char line pos)))
+      (unless (member ch '(#\space #\( #\)))
+        (cons ch (read-symbol-chars line (1+ pos)))))
+    nil))
+
 ;;; Evaluator
 
 (defun misp-eval (token)
-  (cond ((numberp token) token))
-)
+  (cond ((numberp token) token)
+        ((stringp token) token)))
 
 ;;; Output
 
