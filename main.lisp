040f7ca5c31c7837b82ce5e6a006a00d98bfc451
ykskb <yoheikusakabe@gmail.com>
2021-01-11 20:44:00 +0800

    Convert line to class for nested list reading.

--- main.lisp
+++ main.lisp
@@ -7,30 +7,54 @@
 
 (defparameter *stream-in* *standard-input*)
 (defparameter *stream-out* *standard-output*)
+
 (defun misp (&optional (stream-in nil) (stream-out nil))
   (when stream-in
     (setf *stream-in* stream-in))
   (when stream-out
     (setf *stream-out* stream-out))
   (misp-repl))
+
+(defclass input-line (); Tried functional approach, but mutual recursion for reading list ended up ugly...
+  ((str
+    :initarg :str
+    :accessor str)
+   (head
+    :initform 0
+    :accessor head))
+  (:documentation "Input line class with the character head."))
+
+(defmethod has-more-str ((obj input-line))
+  (< (head obj) (length (str obj))))
+
+(defmethod forward-head ((obj input-line))
+  (setf (head obj) (1+ (head obj)))) 
+
+(defmethod peek-ch ((obj input-line))
+  (char (str obj) (head obj)))
+
+(defmethod read-ch ((obj input-line))
+  (let ((ch (peek-ch obj)))
+    (forward-head obj)
+    ch))
   
 (defun misp-repl ()
   (write-string "> " *stream-out*)
   (finish-output)
   (let ((line (clean-line(read-line *stream-in* nil :eof))))
     (unless (equal line "(misp:exit)")
-      (misp-print (misp-eval (misp-read line)))
+      (misp-print (misp-eval (misp-read (make-instance 'input-line :str line))))
       (misp-repl))))
 
 ;;; Parser
 
-(defun misp-read (line &optional (pos 0))
-  (when (< pos (length line))
-    (let ((ch (char line pos)))
-      (cond ((equal ch #\Space) (misp-read line (1+ pos)))
-            ((equal ch #\() (read-list (subseq line (1+ pos))))
-            ((digit-char-p ch) (read-number (subseq line pos)))
-            (t (read-symbol (subseq line pos)))))))
+(defun misp-read (line)
+  (when (has-more-str line)
+    (let ((ch (peek-ch line)))
+      (cond ((equal ch #\Space) (forward-head line) (misp-read line))
+            ((equal ch #\() (forward-head line) (read-list line))
+            ((digit-char-p ch) (read-number line))
+            (t (read-symbol line))))))
 
 (defun clean-line (line)
   (list-to-string 
@@ -47,32 +71,31 @@
         (cons ch (remove-extra-spaces line (1+ pos) nil))))
     nil))
 
-(defun read-list (line &optional (pos 0))
-  (when (and (< pos (length line)) 
-        (not (equal #\) (char line pos))))
-    (let ((token (misp-read (subseq line pos))))
+(defun read-list (line)
+  (if (and (has-more-str line) 
+           (not (equal #\) (peek-ch line))))
+    (let ((token (misp-read line)))
       (if token
-        (cons token (read-list line (1+ (+ pos (num-str-length token)))))
-        nil))))
+        (cons token (read-list line))
+        nil))
+    nil))
 
 (defun read-number (line)
   (parse-integer (list-to-string (read-number-chars line))))
  
-(defun read-number-chars (line &optional (pos 0))
-  (if  (< pos (length line))
-    (let ((ch (char line pos)))
-      (when (digit-char-p ch)
-        (cons ch (read-number-chars line (1+ pos)))))
+(defun read-number-chars (line)
+  (if (has-more-str line)
+    (when (digit-char-p (peek-ch line))
+      (cons (read-ch line) (read-number-chars line)))
     nil))
 
-(defun read-symbol (line &optional (pos 0))
+(defun read-symbol (line)
   (list-to-string (read-symbol-chars line)))
 
-(defun read-symbol-chars (line &optional (pos 0))
-  (if (< pos (length line))
-    (let ((ch (char line pos)))
-      (unless (member ch '(#\space #\( #\)))
-        (cons ch (read-symbol-chars line (1+ pos)))))
+(defun read-symbol-chars (line)
+  (if (has-more-str line)
+    (unless (member (peek-ch line) '(#\space #\( #\)))
+      (cons (read-ch line) (read-symbol-chars line)))
     nil))
 
 ;;; Evaluator
